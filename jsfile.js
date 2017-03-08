7b76d48ba21ca17a8ff9ff45b0e567b156ced302
Vikingweirdo <1017730629@qq.com>
2017-03-08 17:05:56 +0800

    js基本应用
    打印99乘法表

--- jsfile.js
+++ jsfile.js
@@ -1,1 +1,11 @@
-document.write("<h1> use Document write</h1>");
+document.write("<table border = \"18\">");
+	for (i = 1 ;i <= 9;i++)
+	{
+		document.write("<tr>");
+		for (j = 1;j<=i ;j++ )
+		{
+			document.write("<td>" + i +"*" +j+ "="+ i*j +"</td>" );	
+		}
+		document.write("</tr>");
+	}
+document.write("</table>");
